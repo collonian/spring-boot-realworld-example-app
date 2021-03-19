69dd77f5ac53c4aa2208b1f6751547d2a7cece1d
ghost.choe <collonian@hotmail.com>
2021-03-20 08:12:57 +0900

    Create article_history table

---
+++ src/main/resources/db/migration/V2__create_history_table.sql
@@ -0,0 +1,11 @@
+
+create table article_history (
+  id varchar(255) primary key,
+  article_id varchar(255) not null,
+  history_code varchar(255) not null,
+  title varchar(255),
+  description text,
+  body text,
+  created_at TIMESTAMP NOT NULL,
+  primary key(id)
+);
